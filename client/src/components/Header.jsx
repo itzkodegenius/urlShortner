49ad5c3cdb61fade7703a4520e6af282ad8d942c
code alchemy <itzkodegenius@gmail.com>
2024-07-31 14:37:28 +0500

    Update Header.jsx

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -11,7 +11,7 @@ export default function Header() {
   const handleLogout = async () => {
     try {
       const res = await axios.post(
-        "http://localhost:8080/auth/logout",
+        "https://shortlypro.vercel.app/auth/logout",
         {},
         {
           headers: {
